9e1680a8561b0a65e68df09742311f75440842d0
Integral Team V <repo@sfxdx.ru>
2019-09-19 14:28:59 +0700

    rename external tokens template

--- contracts/ExternalTokenTemplate.sol
+++ contracts/ExternalTokenTemplate.sol
@@ -6,8 +6,8 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 contract ExternalTokenTemplate is ERC20 {
     using SafeMath for uint256;
 
-    string public constant name = "Town Token";
-    string public constant symbol = "TTW";
+    string public constant name = "Some Other Token";
+    string public constant symbol = "SOTk";
     uint8 public constant decimals = 18;
 
     constructor (uint256 totalSupply) public {
